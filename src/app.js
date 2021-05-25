423f34d3a16eba019f0947718bb5663303011403
alexbowen <alexbowen123@googlemail.com>
2021-05-25 15:46:45 +0100

    change para,ter keys to match AWS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const { App } = require("@slack/bolt");
 
 const app = new App({
-  token: process.env.BOT_TOKEN,
-  signingSecret: process.env.SIGNING_SECRET
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 module.exports =  app;
